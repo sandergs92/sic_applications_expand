2d6f47e98abbdf39df423c89071ac9739bc8039b
thomasvanorden <thomasvanorden@live.nl>
2023-05-09 16:47:21 +0200

    Fixed imports

--- sic_framework/__init__.py
+++ sic_framework/__init__.py
@@ -1,7 +1,8 @@
-from .core.component_manager_python2 import SICComponentManager, SICSensorManager
+from .core.component_manager_python2 import SICComponentManager
 from .core.service_python2 import SICService
 from .core.actuator_python2 import SICActuator
 from .core.sensor_python2 import SICSensor
+from .core.message_python2 import SICMessage, SICRequest, SICConfMessage
 
 from .core import utils
 from .core import sic_logging
