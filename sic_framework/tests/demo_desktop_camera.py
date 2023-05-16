7c02f1b8045de9da1f71eea4f9857b953c5443fe
thomasvanorden <thomasvanorden@live.nl>
2023-05-16 12:48:43 +0200

    Showing images using queue such that it also works on Mac OS (main thread imshowing)

--- sic_framework/tests/demo_desktop_camera.py
+++ sic_framework/tests/demo_desktop_camera.py
@@ -1,17 +1,21 @@
-import time
-
 import cv2
 from sic_framework.core.message_python2 import CompressedImageMessage
 from sic_framework.devices.desktop.desktop_camera import DesktopCamera
+import queue
 
 """ 
 This demo should display a camera image from your webcam on your laptop.
 """
 
+imgs = queue.Queue()
 def on_image(image_message: CompressedImageMessage):
-  cv2.imshow('', image_message.image)
-  cv2.waitKey(1)
+  imgs.put(image_message.image)
+
 
 camera = DesktopCamera()
 camera.register_callback(on_image)
-time.sleep(10)
+
+while True:
+  img = imgs.get()
+  cv2.imshow('', img)
+  cv2.waitKey(1)
