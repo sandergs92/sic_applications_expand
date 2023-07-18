65d3d6dbfdf4bd4a9508015ec83900eb56b17605
thomas <thomas.wiggers@student.uva.nl>
2023-07-18 16:34:16 +0200

    extended demo

--- sic_framework/tests/demo_text_to_speech_nao_speech.py
+++ sic_framework/tests/demo_text_to_speech_nao_speech.py
@@ -1,4 +1,12 @@
-from sic_framework.devices.common_naoqi.naoqi_text_to_speech import NaoqiTextToSpeech, NaoqiTextToSpeechRequest
+from sic_framework.devices import Nao
+from sic_framework.devices.common_naoqi.naoqi_text_to_speech import NaoqiTextToSpeechRequest
 
-nao_tts = NaoqiTextToSpeech(ip='192.168.0.151')
-nao_tts.request(NaoqiTextToSpeechRequest("Hello!"))
+nao = Nao(ip='192.168.0.151')
+
+# waits for the robot to finish talking
+nao.tts.request(NaoqiTextToSpeechRequest("Hello!"))
+print("Hello!")
+
+# does not wait
+nao.tts.request(NaoqiTextToSpeechRequest("World!"), block=False)
+print("World!")
