ca708b8861270fada54e50d7564f12eac48dc0b8
thomas <thomas.wiggers@student.uva.nl>
2023-07-18 16:33:15 +0200

    fix and cleanup scripts

---
+++ sic_framework/scripts/stop_robot.ps1
@@ -0,0 +1,29 @@
+param (
+    [string]$robot_type = "",
+    [string]$host = ""
+)
+
+###############################################
+# Get hostname and robot name from arguments  #
+###############################################
+
+if (-not $host) {
+    Write-Host "Missing robot ip address -h"
+    exit 1
+}
+
+if (-not $robot_type) {
+    Write-Host "Missing robot type -t (nao or pepper)"
+    exit 1
+}
+
+###############################################
+# Start SIC!                                  #
+###############################################
+
+$sshCommand = "ssh nao@$host `"
+    pkill -f `"python2 ${robot_type}.py`"`""
+
+Invoke-Expression -Command $sshCommand
+
+Write-Host "Done!"
--- sic_framework/scripts/stop_robot.sh
+++ sic_framework/scripts/stop_robot.sh
@@ -6,8 +6,9 @@
 
 unset -v host
 
-while getopts h: opt; do
+while getopts t:h: opt; do
         case $opt in
+                t) robot_type=$OPTARG ;;
                 h) host=$OPTARG ;;
                 *)
                         echo 'Error in command line parsing' >&2
@@ -18,13 +19,18 @@ done
 shift "$(( OPTIND - 1 ))"
 
 : ${host:?Missing robot ip adress -h}
+: ${robot_type:?Missing robot type -t (nao or pepper)}
 
 ###############################################
 # Start SIC!                                  #
 ###############################################
 
 ssh nao@$host << EOF
-  pkill -f "python2 nao.py"
+  pkill -f "python2 ${robot_type}.py"
 EOF
 
 echo "Done!"
+
+
+
+
