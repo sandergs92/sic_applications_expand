4d1307158e04959e6d8013a50a9dda8db4e3f569
thomas <thomas.wiggers@student.uva.nl>
2023-07-25 15:30:26 +0200

    fix ip

--- sic_framework/scripts/copy_robot.ps1
+++ sic_framework/scripts/copy_robot.ps1
@@ -19,7 +19,7 @@ robocopy ../.. framework_tmp /xd venv /xd .git /xd sic_framework /s;
 # Copy sic_framework folder
 robocopy .. framework_tmp\sic_framework /xd services /xd scripts /s; 
 
-scp -r framework_tmp/. nao@10.15.2.168:framework/;
+scp -r framework_tmp/. nao@${host_name}:framework/;
 
 # detelete framework_tmp
 Remove-Item -Path framework_tmp -Recurse -Force;
