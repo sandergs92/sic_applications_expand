8e6923846ddc9e1c9d8391ac1470b827525660be
thomasvanorden <thomasvanorden@live.nl>
2023-11-03 16:52:31 +0100

    Code cleanup

--- sic_framework/tests/demo_nao_speakers.py
+++ sic_framework/tests/demo_nao_speakers.py
@@ -1,18 +1,8 @@
-import json
-import threading
-import time
 import wave
-from typing import io
-
-import pyaudio
-
 from sic_framework.core.message_python2 import AudioMessage, AudioRequest
-from sic_framework.devices import Pepper
-from sic_framework.services.dialogflow.dialogflow import DialogflowConf, GetIntentRequest, Dialogflow, \
-    StopListeningMessage
+from sic_framework.devices import Nao
 
 # Read the wav file
-
 wavefile = wave.open('test_sound_dialogflow.wav', 'rb')
 samplerate = wavefile.getframerate()
 
@@ -23,30 +13,12 @@ print("  data size in bytes:", wavefile.getsampwidth())
 print("  number of chanels:", wavefile.getnchannels())
 print()
 
-pepper = Pepper(ip="192.168.0.197")
 
-
-
-
+nao = Nao(ip="192.168.0.0")
 
 print("Sending audio!")
-
-# bugged
-# sound = wavefile.readframes(wavefile.getnframes())
-# message = AudioRequest(sample_rate=samplerate, waveform=sound)
-# pepper.speaker.request(message)
-
-# print("Audio is done playing!")
-
-
 sound = wavefile.readframes(wavefile.getnframes())
 message = AudioMessage(sample_rate=samplerate, waveform=sound)
-pepper.speaker.send_message(message)
+nao.speaker.send_message(message)
 
 print("Audio sent, without waiting for it to complete playing.")
-
-
-
-
-
-
