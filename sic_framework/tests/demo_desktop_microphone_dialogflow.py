16803e012a99c934b809c75827af9ead3ce70dd6
Ting-Chia Chiang <t.chiang@vu.nl>
2023-11-06 17:44:15 +0100

    add the demo dialogflow for local desktop setup

---
+++ sic_framework/tests/demo_desktop_microphone_dialogflow.py
@@ -0,0 +1,50 @@
+import json
+import numpy as np
+
+from sic_framework.devices.desktop import Desktop
+from sic_framework.services.dialogflow.dialogflow import (DialogflowConf, GetIntentRequest, RecognitionResult,
+                                                          QueryResult, Dialogflow)
+""" 
+This demo should have your local microphone picking up your intent and printing out the reply from Dialogflow
+
+The Dialogflow should be running. You can start it with:
+[services/dialogflow] python dialogflow.py
+"""
+
+# the callback function
+def on_dialog(message):
+    if message.response:
+        if message.response.recognition_result.is_final:
+            print("Transcript:", message.response.recognition_result.transcript)
+
+# local desktop setup
+desktop = Desktop()
+
+# load the key json file
+keyfile_json = json.load(open("dialogflow-tutorial.json"))
+
+# set up the config
+conf = DialogflowConf(keyfile_json=keyfile_json, sample_rate_hertz=44100, language="en")
+
+# initiate Dialogflow object
+dialogflow = Dialogflow(ip='localhost', conf=conf)
+
+# connect the output of DesktopMicrophone as the input of DialogflowComponent
+dialogflow.connect(desktop.mic)
+
+# register a callback function to act upon arrival of recognition_result
+dialogflow.register_callback(on_dialog)
+
+# Demo starts
+print(" -- Ready -- ")
+x = np.random.randint(10000)
+
+for i in range(25):
+    print(" ----- Conversation turn", i)
+    reply = dialogflow.request(GetIntentRequest(x))
+
+    print("The detected intent:", reply.intent)
+
+    if reply.fulfillment_message:
+        text = reply.fulfillment_message
+        print("Reply:", text)
