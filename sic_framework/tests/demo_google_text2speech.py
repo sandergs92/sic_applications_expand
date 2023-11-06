464fa500f0a674b7fcb9f1b06a259b12ee4e2b50
thomasvanorden <thomasvanorden@live.nl>
2023-11-06 13:16:34 +0100

    Added link to docs and placeholder for keyfile

--- sic_framework/tests/demo_google_text2speech.py
+++ sic_framework/tests/demo_google_text2speech.py
@@ -8,11 +8,14 @@ from sic_framework.services.text2speech.text2speech_service import Text2Speech, 
 """
 Google Text2Speech service should be running. You can start it with:
 [services/text2speech] python text2speech_service.py 
+
+NOTE: you need to have setup Cloud Text-to-Speech API in your Google Cloud Console and configure the credential keyfile.
+See https://console.cloud.google.com/apis/api/texttospeech.googleapis.com/ 
 """
 
 # nao = Nao("192.168.0.191")
 
-tts_conf = Text2SpeechConf(keyfile="/Users/thomasvanorden/Documents/Carrière/Interactive_Robotics/sail-393209-95cba17732fb.json")
+tts_conf = Text2SpeechConf(keyfile="your_keyfile.json")
 tts = Text2Speech(conf=tts_conf)
 reply = tts.request(GetSpeechRequest("Hello, how are you?"))
 
