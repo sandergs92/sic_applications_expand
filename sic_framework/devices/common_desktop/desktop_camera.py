8bc5f72fa12c1f73fd1d133c025ba26ba18e0b95
thomasvanorden <thomasvanorden@live.nl>
2023-11-14 13:19:52 +0100

    Added resize from conf

--- sic_framework/devices/common_desktop/desktop_camera.py
+++ sic_framework/devices/common_desktop/desktop_camera.py
@@ -8,6 +8,21 @@ from sic_framework.core.message_python2 import CompressedImageMessage, SICConfMe
 from sic_framework.core.sensor_python2 import SICSensor
 
 
+class DesktopCameraConf(SICConfMessage):
+    def __init__(self, fx=1.0, fy=1.0):
+        """
+        Sets desktop camera configuration parameters.
+
+        See https://docs.opencv.org/4.x/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
+        :param fx: rescaling factor along x-axis (float)
+        :param fy: rescaling factor along y-axis (float)
+        """
+        SICConfMessage.__init__(self)
+
+        self.fx = fx
+        self.fy = fy
+
+
 class DesktopCameraSensor(SICSensor):
     def __init__(self, *args, **kwargs):
         super(DesktopCameraSensor, self).__init__(*args, **kwargs)
@@ -19,7 +34,7 @@ class DesktopCameraSensor(SICSensor):
 
     @staticmethod
     def get_conf():
-        return SICConfMessage()
+        return DesktopCameraConf()
 
     @staticmethod
     def get_inputs():
@@ -31,6 +46,8 @@ class DesktopCameraSensor(SICSensor):
 
     def execute(self):
         ret, frame = self.cam.read()
+        frame = cv2.resize(frame, (0, 0), fx=self.params.fx, fy=self.params.fy)
+
         if not ret:
             self.logger.warning("Failed to grab frame from video device")
 
