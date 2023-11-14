8aa769bda308935714c30f0e713ed4a7e91a6bb6
thomasvanorden <thomasvanorden@live.nl>
2023-11-14 13:20:40 +0100

    Queue using maxsize and resize conf

--- sic_framework/tests/demo_desktop_camera_facerecognition.py
+++ sic_framework/tests/demo_desktop_camera_facerecognition.py
@@ -5,6 +5,7 @@ import cv2
 from sic_framework.core.message_python2 import BoundingBoxesMessage
 from sic_framework.core.message_python2 import CompressedImageMessage
 from sic_framework.core.utils_cv2 import draw_bbox_on_image
+from sic_framework.devices.common_desktop.desktop_camera import DesktopCameraConf
 from sic_framework.devices.desktop import Desktop
 from sic_framework.services.face_recognition_dnn.face_recognition import DNNFaceRecognition
 
@@ -12,30 +13,22 @@ from sic_framework.services.face_recognition_dnn.face_recognition import DNNFace
 This demo recognizes faces from your webcam and displays the result on your laptop.
 """
 
-imgs_buffer = queue.Queue()
+imgs_buffer = queue.Queue(maxsize=1)
 
 
 def on_image(image_message: CompressedImageMessage):
-    try:
-        imgs_buffer.get_nowait()  # remove previous message if its still there
-    except queue.Empty:
-        pass
     imgs_buffer.put(image_message.image)
 
 
-faces_buffer = queue.Queue()
+faces_buffer = queue.Queue(maxsize=1)
 
 
 def on_faces(message: BoundingBoxesMessage):
-    try:
-        faces_buffer.get_nowait()  # remove previous message if its still there
-    except queue.Empty:
-        pass
     faces_buffer.put(message.bboxes)
 
 
 # Connect to the services
-desktop = Desktop()
+desktop = Desktop(camera_conf=DesktopCameraConf(fx=0.3, fy=0.3))
 face_rec = DNNFaceRecognition()
 
 # Feed the camera images into the face recognition component
