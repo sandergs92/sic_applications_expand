5d72a569c73701e898bce26c34dab352500ce177
Ting-Chia Chiang <t.chiang@vu.nl>
2023-11-30 13:38:41 +0100

    Set pitch, speed, volume paremeters in the demo

--- sic_framework/tests/demo_text_to_speech_nao_speech.py
+++ sic_framework/tests/demo_text_to_speech_nao_speech.py
@@ -4,9 +4,9 @@ from sic_framework.devices.common_naoqi.naoqi_text_to_speech import NaoqiTextToS
 nao = Nao(ip='192.168.0.151')
 
 # waits for the robot to finish talking
-nao.tts.request(NaoqiTextToSpeechRequest("Hello!"))
+nao.tts.request(NaoqiTextToSpeechRequest("Hello!", pitch_shift=0.5))
 print("Hello!")
 
 # does not wait
-nao.tts.request(NaoqiTextToSpeechRequest("World!"), block=False)
-print("World!")
+nao.tts.request(NaoqiTextToSpeechRequest("Wereld!", pitch_shift=1.0, volume=1.5, language="Dutch", speed=50), block=False)
+print("Wereld!")
