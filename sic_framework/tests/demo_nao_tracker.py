d53353842be879c7a6dcbb07ea30ad316b1d7bed
Ting-Chia Chiang <t.chiang@vu.nl>
2024-01-04 12:53:11 +0100

    Update the demo for tracking a face and a red ball with whole-body movement and end-effectorsUpdate the demo for tracking a face and a red ball with whole body movement and end effectors

--- sic_framework/tests/demo_nao_tracker.py
+++ sic_framework/tests/demo_nao_tracker.py
@@ -1,19 +1,35 @@
 import time
 from sic_framework.devices import Nao
-from sic_framework.devices.common_naoqi.naoqi_tracker import RegisterTargetRequest, StartTrackRequest, StopTrackRequest
+from sic_framework.devices.common_naoqi.naoqi_tracker import StartTrackRequest, StopAllTrackRequest, RemoveTargetRequest
+from sic_framework.devices.common_naoqi.naoqi_motion import NaoPostureRequest
+
+"""
+This demo shows you how to make Nao track a face, walk to you given a position relative to the face,
+and later move an end-effector (both arms in this case) to track a red ball
+"""
 
 # Connect to NAO
-nao = Nao(ip="192.168.0.0")
+nao = Nao(ip="192.168.0.165")
 
-# Add target to track.
+# start tracking a face
 target_name = "Face"
-nao.tracker.request(RegisterTargetRequest(target_name, 0.3))
-
-# Start tracking
-nao.tracker.request(StartTrackRequest(target_name))
+# set the robot relative position to target
+# the robot stays a 30 centimeters of target with 10 cm precision
+move_rel_position = [-0.3, 0.0, 0.0, 0.1, 0.1, 0.1]
+nao.tracker.request(StartTrackRequest(target_name, 0.2, "Move", effector="None", move_rel_position=move_rel_position))
 
 # Do some stuff here
-time.sleep(10)
+time.sleep(20)
 
-# Stop tracking
-nao.tracker.request(StopTrackRequest())
+# unregister target face
+nao.tracker.request(RemoveTargetRequest(target_name))
+
+# start tracking a red ball
+target_name = "RedBall"
+nao.tracker.request(StartTrackRequest(target_name, 0.06, "Head", effector="Arms"))
+
+# Do some stuff here
+time.sleep(20)
+
+# Stop tracking everything
+nao.tracker.request(StopAllTrackRequest())
