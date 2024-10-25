53bb0385421c3af7949bc4b2e373ec1bec5c3f64
Ting-Chia Chiang <t.chiang@vu.nl>
2024-10-25 12:38:37 +0200

    Add demo_desktop_microphone_dialogflow.py

---
+++ demos/desktop/demo_desktop_microphone_dialogflow.py
@@ -0,0 +1,72 @@
+import json
+
+import numpy as np
+from sic_framework.devices.desktop import Desktop
+from sic_framework.services.dialogflow.dialogflow import (
+    Dialogflow,
+    DialogflowConf,
+    GetIntentRequest,
+    QueryResult,
+    RecognitionResult,
+)
+
+"""
+This demo should have Nao picking up your intent and replying according to your trained agent using dialogflow.
+
+IMPORTANT
+
+First, you need to obtain your own keyfile.json from Dialogflow and place it in a location that the code at line 39 can load.
+How to get a key? See https://socialrobotics.atlassian.net/wiki/spaces/CBSR/pages/2205155343/Getting+a+google+dialogflow+key for more information.
+
+Second, the Dialogflow service needs to be running:
+
+1. pip install social-interaction-cloud[dialogflow]
+2. run-dialogflow
+
+"""
+
+
+# the callback function
+def on_dialog(message):
+    if message.response:
+        if message.response.recognition_result.is_final:
+            print("Transcript:", message.response.recognition_result.transcript)
+
+
+# local desktop setup
+desktop = Desktop()
+
+# load the key json file
+keyfile_json = json.load(open("dialogflow-tutorial.json"))
+
+# set up the config
+conf = DialogflowConf(keyfile_json=keyfile_json, sample_rate_hertz=44100, language="en")
+
+# initiate Dialogflow object
+dialogflow = Dialogflow(ip="localhost", conf=conf)
+
+# connect the output of DesktopMicrophone as the input of DialogflowComponent
+dialogflow.connect(desktop.mic)
+
+# register a callback function to act upon arrival of recognition_result
+dialogflow.register_callback(on_dialog)
+
+# Demo starts
+print(" -- Ready -- ")
+x = np.random.randint(10000)
+
+try:
+    for i in range(25):
+        print(" ----- Conversation turn", i)
+        # create context_name-lifespan pairs. If lifespan is set to 0, the context expires immediately
+        contexts_dict = {"name": 1}
+        reply = dialogflow.request(GetIntentRequest(x, contexts_dict))
+
+        print("The detected intent:", reply.intent)
+
+        if reply.fulfillment_message:
+            text = reply.fulfillment_message
+            print("Reply:", text)
+except KeyboardInterrupt:
+    print("Stop the dialogflow component.")
+    dialogflow.stop()
