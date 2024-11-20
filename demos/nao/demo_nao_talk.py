ab103672445b4af341998b801151b9a0ddc56435
Mike Ligthart <m.e.u.ligthart@vu.nl>
2024-11-20 13:56:29 +0100

    extends nao talk demo to include animated say and doing gestures and speaking in parallel using block=False

--- demos/nao/demo_nao_talk.py
+++ demos/nao/demo_nao_talk.py
@@ -1,8 +1,45 @@
+from time import sleep
+
 from sic_framework.devices import Nao
+from sic_framework.devices.common_naoqi.naoqi_autonomous import NaoWakeUpRequest, NaoRestRequest
+from sic_framework.devices.common_naoqi.naoqi_motion import NaoqiAnimationRequest
 from sic_framework.devices.common_naoqi.naoqi_text_to_speech import (
     NaoqiTextToSpeechRequest,
 )
 
-nao = Nao(ip="192.168.2.7")  # adjust this to the IP adress of your robot.
 
-nao.tts.request(NaoqiTextToSpeechRequest("Hello!"))
+class NaoTalkDemo:
+
+    def __init__(self, ip: str):
+        self.nao = Nao(ip=ip)  # adjust this to the IP address of your robot.
+
+    def say(self):
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Say."))
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Hello, I am a Nao robot!"))
+
+    def say_animated(self):
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Animated Say."))
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Hello, I am a Nao robot! And I like to chat.", animated=True))
+
+    def say_with_gesture(self):
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Say and gesture."))
+        self.nao.tts.request(NaoqiTextToSpeechRequest("Hello, I am a Nao robot! And I like to chat."), block=False)
+        self.nao.motion.request(NaoqiAnimationRequest("animations/Stand/Gestures/Hey_1"))
+
+    def wakeup(self):
+        self.nao.autonomous.request(NaoWakeUpRequest())
+
+    def rest(self):
+        self.nao.autonomous.request(NaoRestRequest())
+
+
+if __name__ == '__main__':
+    nao_talk = NaoTalkDemo(ip="10.0.0.?")
+    nao_talk.wakeup()
+    nao_talk.say()
+    sleep(2)
+    nao_talk.say_animated()
+    sleep(2)
+    nao_talk.say_with_gesture()
+    sleep(2)
+    nao_talk.rest()
